075337dc30077e0bee87204f9b3a220a5d2a5c4d
Jorge Garcia <garciahirota@gmail.com>
2013-07-09 14:34:02 -0500

    modify web.js

--- web.js
+++ web.js
@@ -3,7 +3,15 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	var fs = require("fs");
+	
+	fs.readFile('index.html', function (err, data) {
+		if (err) throw err;
+		response.send(data.toString());
+	});
+
+
+	
 });
 
 var port = process.env.PORT || 5000;
